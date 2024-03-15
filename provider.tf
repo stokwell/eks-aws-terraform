cdb6c4dd1edb05ff13853554f21f1ab8b573b9cc
Vitaly Lyapunov <stokwell1@gmail.com>
2024-03-15 13:16:24 +0100

    Creates AWS VPC in eu-cental-1 region

---
+++ provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  profile = "terraform"
+  region  = "eu-central-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
